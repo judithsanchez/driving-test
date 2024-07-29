5b4ee33de5ba255f802a7c48a0a6ff3e7ac9cb31
judithsanchez <judithv.sanchezc@gmail.com>
2024-07-30 00:08:17 +0200

    fix: :monocle_face: Fixing data

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'http://localhost:3000/questions';
-export const NUM_QUESTIONS = 10;
+export const NUM_QUESTIONS = 33;
 export const DRIVING_TEST_TITLE = 'Driving Test';
 export const CHECK_ANSWER_TEXT = 'Check Answer';
 export const CORRECT_ANSWER_TEXT = 'Correct answer:';
